2638f0e19fca1982c2dd3a671f3f224ed041f67e
Sidharth Arya <code@sidhartharya.com>
2023-07-10 18:54:50 +0530

    Auto Commit: Mon Jul 10 18:54:50 IST 2023

--- init.lua
+++ init.lua
@@ -61,7 +61,8 @@ lazy.setup({
 	{'nvim-treesitter/nvim-treesitter'},
 	{'voldikss/vim-floaterm'},
 	{'Mofiqul/vscode.nvim'},
-	{'nvim-tree/nvim-tree.lua'}
+	{'nvim-tree/nvim-tree.lua'},
+	{'ggandor/leap.nvim'}
 })
 
 require('lualine').setup{
@@ -281,6 +282,29 @@ lspconfig.rust_analyzer.setup {
     ['rust-analyzer'] = {},
   },
 }
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+require'lspconfig'.lua_ls.setup{}
 
 
 -- Global mappings.
@@ -479,3 +503,8 @@ require("nvim-tree").setup({
   },
 })
 vim.keymap.set('n', '<space>fb', '<cmd>NvimTreeToggle<cr>', {})
+
+--- Leap
+-- require('leap').add_default_mappings()
+--
+
