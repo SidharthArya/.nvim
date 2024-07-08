71060c4358fcfc52085a8f2ed7d881cca67f2b80
Sidharth Arya <code@sidhartharya.com>
2024-07-09 01:02:24 +0530

    Comment + Surround

--- lua/modules/core.lua
+++ lua/modules/core.lua
@@ -113,4 +113,20 @@ return {{
       require("base46").load_all_highlights()
     end,
   },
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    }
+},
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+}
 }
