b076c49580edde2af3447fa61cf020908503e808
Sidharth Arya <code@sidhartharya.com>
2024-07-11 02:45:25 +0530

    Fomat

--- init.lua
+++ init.lua
@@ -1,14 +1,28 @@
 packages = {}
 
-local modules = {"core", "keys", "appearance", "completion", "vc", "files", "projects", "mason", "programming", "lsp", "terminal",
-                 "dashboard", "lazy"}
+local modules = {
+  "core",
+  "ui",
+  "keys",
+  "appearance",
+  "completion",
+  "vc",
+  "files",
+  "projects",
+  "mason",
+  "programming",
+  "lsp",
+  "dap",
+  "terminal",
+  "dashboard",
+  "lazy"
+}
 
 for _, module in ipairs(modules) do
-    local mout = (require('modules.' .. module))
-    if type(mout) == 'table' then
-        for _, v in ipairs(mout) do
-            table.insert(packages, v)
-        end
+  local mout = (require('modules.' .. module))
+  if type(mout) == 'table' then
+    for _, v in ipairs(mout) do
+      table.insert(packages, v)
     end
+  end
 end
-
