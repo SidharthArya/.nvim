8702a6ca7de9da41f495404b3152d79c04cf77d7
Sidharth Arya <code@sidhartharya.com>
2024-07-14 06:20:02 +0530

    Appearance Theme vscode

--- lua/modules/appearance.lua
+++ lua/modules/appearance.lua
@@ -1,25 +1,26 @@
 return {
-	{
-		"ellisonleao/gruvbox.nvim",
-  	config = function()
-	vim.opt.background = 'dark'
-	vim.opt.termguicolors = true
-	vim.cmd('colorscheme gruvbox')
 
-  	end,
-  	requires = {'nvim-tree/nvim-web-devicons'}
-	},
+  {
+    'Mofiqul/vscode.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      vim.opt.background = 'dark'
+      vim.opt.termguicolors = true
+      vim.cmd('colorscheme vscode')
+    end
+  },
 
-	{
-		'nvim-lualine/lualine.nvim',
-	requires = { 'nvim-tree/nvim-web-devicons'},
-	config = function()
-		require('lualine').setup()
-	end
-},
-  {'romgrk/barbar.nvim',
+  {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup()
+    end
+  },
+  {
+    'romgrk/barbar.nvim',
     dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
     init = function() vim.g.barbar_auto_setup = false end,
