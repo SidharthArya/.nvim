4ad0299657b1351e9b11407676b0ab4e907c8753
Sidharth Arya <code@sidhartharya.com>
2024-07-25 12:38:53 +0530

    Formatting and Linting

--- lua/modules/programming.lua
+++ lua/modules/programming.lua
@@ -1,263 +1,318 @@
-return {{
-    'stevearc/conform.nvim',
-    opts = {}
-}, -- {
---   'mfussenegger/nvim-lint',
---   opts = {}
--- }
-{
-    "SmiteshP/nvim-navic",
-    config = function()
-      require('nvim-navic').setup {
-    icons = {
-        File          = "󰈙 ",
-        Module        = " ",
-        Namespace     = "󰌗 ",
-        Package       = " ",
-        Class         = "󰌗 ",
-        Method        = "󰆧 ",
-        Property      = " ",
-        Field         = " ",
-        Constructor   = " ",
-        Enum          = "󰕘",
-        Interface     = "󰕘",
-        Function      = "󰊕 ",
-        Variable      = "󰆧 ",
-        Constant      = "󰏿 ",
-        String        = "󰀬 ",
-        Number        = "󰎠 ",
-        Boolean       = "◩ ",
-        Array         = "󰅪 ",
-        Object        = "󰅩 ",
-        Key           = "󰌋 ",
-        Null          = "󰟢 ",
-        EnumMember    = " ",
-        Struct        = "󰌗 ",
-        Event         = " ",
-        Operator      = "󰆕 ",
-        TypeParameter = "󰊄 ",
+return { {
+  'stevearc/conform.nvim',
+  keys = {
+    {
+      "<Space>bf",
+      function()
+        require('conform').format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        })
+      end,
+      desc = "Format buffer"
     },
-    lsp = {
-        auto_attach = true,
-        preference = nil,
-    },
-    highlight = false,
-    separator = " > ",
-    depth_limit = 0,
-    depth_limit_indicator = "..",
-    safe_output = true,
-    lazy_update_context = false,
-    click = false,
-    format_text = function(text)
-        return text
-    end,
-}
-
-    end
+  },
+  opts = {},
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        nix = { "nixfmt" },
+        json = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        graphql = { "prettier" },
+        python = { "isort", "black" }
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false
+      }
+    });
+  end
 },
-{
+  {
+    'mfussenegger/nvim-lint',
+    opts = {},
+    keys = {
+    {
+      "<Space>be",
+      function()
+        require('lint').try_lint()
+      end,
+      desc = "Format buffer"
+    },
+    },
+    config = function()
+      local lint = require("lint");
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        python = { "pylint" }
+      };
+    end
+  },
+  -- {
+  --     "SmiteshP/nvim-navic",
+  --     lazy = false,
+  --     config = function()
+  --       require('nvim-navic').setup {
+  --     icons = {
+  --         File          = "󰈙 ",
+  --         Module        = " ",
+  --         Namespace     = "󰌗 ",
+  --         Package       = " ",
+  --         Class         = "󰌗 ",
+  --         Method        = "󰆧 ",
+  --         Property      = " ",
+  --         Field         = " ",
+  --         Constructor   = " ",
+  --         Enum          = "󰕘",
+  --         Interface     = "󰕘",
+  --         Function      = "󰊕 ",
+  --         Variable      = "󰆧 ",
+  --         Constant      = "󰏿 ",
+  --         String        = "󰀬 ",
+  --         Number        = "󰎠 ",
+  --         Boolean       = "◩ ",
+  --         Array         = "󰅪 ",
+  --         Object        = "󰅩 ",
+  --         Key           = "󰌋 ",
+  --         Null          = "󰟢 ",
+  --         EnumMember    = " ",
+  --         Struct        = "󰌗 ",
+  --         Event         = " ",
+  --         Operator      = "󰆕 ",
+  --         TypeParameter = "󰊄 ",
+  --     },
+  --     lsp = {
+  --         auto_attach = true,
+  --         preference = nil,
+  --     },
+  --     highlight = false,
+  --     separator = " > ",
+  --     depth_limit = 0,
+  --     depth_limit_indicator = "..",
+  --     safe_output = true,
+  --     lazy_update_context = false,
+  --     click = false,
+  --     format_text = function(text)
+  --         return text
+  --     end,
+  -- }
+  --
+  --     end
+  -- },
+  {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
-    dependencies = {{
-        -- snippet plugin
-        "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets",
-        opts = {
-            history = true,
-            updateevents = "TextChanged,TextChangedI"
-        },
-        config = function(_, opts)
-            require("luasnip.loaders.from_vscode").lazy_load {
-                exclude = vim.g.vscode_snippets_exclude or {}
-            }
-            require("luasnip.loaders.from_vscode").lazy_load {
-                paths = vim.g.vscode_snippets_path or ""
-            }
+    dependencies = { {
+      -- snippet plugin
+      "L3MON4D3/LuaSnip",
+      dependencies = "rafamadriz/friendly-snippets",
+      opts = {
+        history = true,
+        updateevents = "TextChanged,TextChangedI"
+      },
+      config = function(_, opts)
+        require("luasnip.loaders.from_vscode").lazy_load {
+          exclude = vim.g.vscode_snippets_exclude or {}
+        }
+        require("luasnip.loaders.from_vscode").lazy_load {
+          paths = vim.g.vscode_snippets_path or ""
+        }
 
-            -- snipmate format
-            require("luasnip.loaders.from_snipmate").load()
-            require("luasnip.loaders.from_snipmate").lazy_load {
-                paths = vim.g.snipmate_snippets_path or ""
-            }
+        -- snipmate format
+        require("luasnip.loaders.from_snipmate").load()
+        require("luasnip.loaders.from_snipmate").lazy_load {
+          paths = vim.g.snipmate_snippets_path or ""
+        }
 
-            -- lua format
-            require("luasnip.loaders.from_lua").load()
-            require("luasnip.loaders.from_lua").lazy_load {
-                paths = vim.g.lua_snippets_path or ""
-            }
+        -- lua format
+        require("luasnip.loaders.from_lua").load()
+        require("luasnip.loaders.from_lua").lazy_load {
+          paths = vim.g.lua_snippets_path or ""
+        }
 
-            vim.api.nvim_create_autocmd("InsertLeave", {
-                callback = function()
-                    if require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()] and
-                        not require("luasnip").session.jump_active then
-                        require("luasnip").unlink_current()
-                    end
-                end
-            })
-            require("luasnip").config.set_config(opts)
-        end
+        vim.api.nvim_create_autocmd("InsertLeave", {
+          callback = function()
+            if require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()] and
+                not require("luasnip").session.jump_active then
+              require("luasnip").unlink_current()
+            end
+          end
+        })
+        require("luasnip").config.set_config(opts)
+      end
     }, -- autopairing of (){}[] etc
-    {
+      {
         "windwp/nvim-autopairs",
         opts = {
-            fast_wrap = {},
-            disable_filetype = {"TelescopePrompt", "vim"}
+          fast_wrap = {},
+          disable_filetype = { "TelescopePrompt", "vim" }
         },
         config = function(_, opts)
-            require("nvim-autopairs").setup(opts)
+          require("nvim-autopairs").setup(opts)
 
-            -- setup cmp for autopairs
-            local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-            require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+          -- setup cmp for autopairs
+          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
         end
-    }, -- cmp sources plugins
-    {"saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer",
-     "hrsh7th/cmp-path"}},
+      }, -- cmp sources plugins
+      { "saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path" } },
     opts = function()
       local cmp = require "cmp"
 
 
-local cmp_ui =  {
-      icons = true,
-      lspkind_text = true,
-      style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-    }
-local cmp_style = cmp_ui.style
+      local cmp_ui = {
+        icons = true,
+        lspkind_text = true,
+        style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+      }
+      local cmp_style = cmp_ui.style
 
-local field_arrangement = {
-  atom = { "kind", "abbr", "menu" },
-  atom_colored = { "kind", "abbr", "menu" },
-}
+      local field_arrangement = {
+        atom = { "kind", "abbr", "menu" },
+        atom_colored = { "kind", "abbr", "menu" },
+      }
 
--- local formatting_style = {
---   -- default fields order i.e completion word + item.kind + item.kind icons
-  -- fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
-  --
-  -- format = function(_, item)
-  --   local icons = require "nvchad.icons.lspkind"
-  --   local icon = (cmp_ui.icons and icons[item.kind]) or ""
-  --
-  --   if cmp_style == "atom" or cmp_style == "atom_colored" then
-  --     icon = " " .. icon .. " "
-  --     item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
-  --     item.kind = icon
-  --   else
-  --     icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
-  --     item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
-  --   end
-  --
-  --   return item
-  -- end,
--- }
+      -- local formatting_style = {
+      --   -- default fields order i.e completion word + item.kind + item.kind icons
+      -- fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
+      --
+      -- format = function(_, item)
+      --   local icons = require "nvchad.icons.lspkind"
+      --   local icon = (cmp_ui.icons and icons[item.kind]) or ""
+      --
+      --   if cmp_style == "atom" or cmp_style == "atom_colored" then
+      --     icon = " " .. icon .. " "
+      --     item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
+      --     item.kind = icon
+      --   else
+      --     icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
+      --     item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
+      --   end
+      --
+      --   return item
+      -- end,
+      -- }
 
-local function border(hl_name)
-  return {
-    { "╭", hl_name },
-    { "─", hl_name },
-    { "╮", hl_name },
-    { "│", hl_name },
-    { "╯", hl_name },
-    { "─", hl_name },
-    { "╰", hl_name },
-    { "│", hl_name },
-  }
-end
-
-local options = {
-  completion = {
-    completeopt = "menu,menuone",
-  },
-
-  window = {
-    completion = {
-      side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
-      winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
-      scrollbar = false,
-    },
-    documentation = {
-      border = border "CmpDocBorder",
-      winhighlight = "Normal:CmpDoc",
-    },
-  },
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-
-  -- formatting = formatting_style,
-  --
-  mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-
-    ["<CR>"] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    },
-
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "nvim_lua" },
-    { name = "path" },
-  },
-}
-
-if cmp_style ~= "atom" and cmp_style ~= "atom_colored" then
-  options.window.completion.border = border "CmpBorder"
-end
-
-return options
-    end,
-    config = function(_, opts)
-        require("cmp").setup(opts)
-    end
-}, {
-    "nvim-treesitter/nvim-treesitter",
-    event = {"BufReadPost", "BufNewFile"},
-    cmd = {"TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo"},
-    build = ":TSUpdate",
-    opts = function()
+      local function border(hl_name)
         return {
-            ensure_installed = {"lua", "luadoc", "printf", "vim", "vimdoc"},
-
-            highlight = {
-                enable = true,
-                use_languagetree = true
-            },
-
-            indent = {
-                enable = true
-            }
+          { "╭", hl_name },
+          { "─", hl_name },
+          { "╮", hl_name },
+          { "│", hl_name },
+          { "╯", hl_name },
+          { "─", hl_name },
+          { "╰", hl_name },
+          { "│", hl_name },
         }
+      end
+
+      local options = {
+        completion = {
+          completeopt = "menu,menuone",
+        },
+
+        window = {
+          completion = {
+            side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
+            winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
+            scrollbar = false,
+          },
+          documentation = {
+            border = border "CmpDocBorder",
+            winhighlight = "Normal:CmpDoc",
+          },
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+
+        -- formatting = formatting_style,
+        --
+        mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+
+          ["<CR>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+          },
+
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif require("luasnip").expand_or_jumpable() then
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif require("luasnip").jumpable(-1) then
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "nvim_lua" },
+          { name = "path" },
+        },
+      }
+
+      if cmp_style ~= "atom" and cmp_style ~= "atom_colored" then
+        options.window.completion.border = border "CmpBorder"
+      end
+
+      return options
     end,
     config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
+      require("cmp").setup(opts)
     end
-}}
+  }, {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+  build = ":TSUpdate",
+  opts = function()
+    return {
+      ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
+
+      highlight = {
+        enable = true,
+        use_languagetree = true
+      },
+
+      indent = {
+        enable = true
+      }
+    }
+  end,
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end
+} }
